e27e9ae09a164ee97ceb7b2093ab4493db0f6874
Ubuntu <ubuntu@ip-172-31-6-82.us-west-2.compute.internal>
2022-10-13 07:41:16 +0000

    Removing a trailing / when defining the endpoint

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
-callback_response: dict = {"message": "Lise"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
-@app.post("/callback/")
+@app.post("/callback")
 async def callback(code: str):
-    resp_str = f"You sent {code}"
+    resp_str = f"[POST] You sent {code}"
+    return {"Response": resp_str}
+
+@app.get("/callback")
+async def callback(code: str):
+    resp_str = f"[GET] You sent {code}"
     return {"Response": resp_str}
