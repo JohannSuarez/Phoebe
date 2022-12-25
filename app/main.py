e923763ecdddda5a5570771eba134fb1c305c564
Johann Suarez <johann.suarez92@gmail.com>
2022-12-25 01:35:17 -0800

    Testing reading and writing into db

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from .utils.app_exceptions import AppExceptionCase
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from .schemas.pkce import PKCEItemCreate
+from .utils.app_exceptions import AppExceptionCase
 from .routers import auth_url, pkce
 from .config.database import create_tables
 from dotenv import dotenv_values
@@ -16,6 +17,8 @@ from .utils.request_exceptions import (
 
 from .utils.app_exceptions import app_exception_handler
 
+import base64
+
 
 create_tables()
 
@@ -23,10 +26,10 @@ config = dotenv_values(".env")
 # These variables have to be checked otherwise, we raise an Exception
 # because the app is useless without these.
 # Create a custom error for these.
-client_id: str = config["CLIENT_ID"] or ""
-client_secret: str = config["CLIENT_SECRET"] or ""
+cl_id: str = config["CLIENT_ID"] or ""
+cl_secret: str = config["CLIENT_SECRET"] or ""
 
-if not (client_id and client_secret):
+if not (cl_id and cl_secret):
     raise Exception("Missing env vars. Please check the .env file on project root.")
 
 app = FastAPI()
@@ -38,6 +41,18 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+'''
+from .schemas.pkce import PKCEItemCreate
+pkce_dict = {"state": "wisemensay", "code_verifier": "onlyfoolsrushin"} 
+pkce.non_endpoint_create_pkce(PKCEItemCreate(**pkce_dict))
+
+pkce_dict2 = {"state": "buticanthelp", "code_verifier": "fallinginlovewithyou"} 
+pkce.non_endpoint_create_pkce(PKCEItemCreate(**pkce_dict2))
+
+# This works, despite the error.
+print(pkce.non_endpoint_find_state("dela").code_verifier) 
+'''
 
 @app.exception_handler(StarletteHTTPException)
 async def custom_http_exception_handler(request, e):
@@ -53,9 +68,36 @@ async def custom_validation_exception_handler(request, e):
 async def custom_app_exception_handler(request, e):
     return await app_exception_handler(request, e)
 
-app.include_router(pkce.router)
-
 @app.get("/callback")
 async def callback(code: str, state: str):
+    '''
+    The callback looks for the received state in the on-disk database.
+    If it finds the state, it gets the associated code_verifier.
+    The code verifier is then used to compose the request to get
+    the access and refresh tokens.
+
+    The following are what's needed to make the token request:
+
+        For the authorization header:
+            "Basic " + base64encode(client_id + ":" + client_secret)
+
+        client_id ( sourced from .env )
+        auth_code ( the authentication code )
+        code_verifier
+
+    Please refer heavily to this doc:
+    https://dev.fitbit.com/build/reference/web-api/troubleshooting-guide/oauth2-tutorial/?clientEncodedId=238N67&redirectUri=https://johanns.xyz/fitbitapp/callback&applicationType=SERVER
+    '''
     resp_str = f"CODE = {code}, STATE = {state}"
+
+    client_id: str = cl_id
+    client_secret: str = cl_secret
+
+    basic_token: str = client_id + ":" + client_secret
+
+    authorization_header: str = f"Basic {base64.b64encode(basic_token.encode('utf-8')).decode('utf-8')}"
+    auth_code: str = code
+    code_verifier: str = pkce.non_endpoint_find_state(state).code_verifier # type: ignore
+
+
     return {"Response": resp_str}
--- app/routers/pkce.py
+++ app/routers/pkce.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine
 
 from ..services.pkce import PKCEService
 from ..schemas.pkce import PKCEItem, PKCEItemCreate
@@ -12,6 +14,34 @@ router = APIRouter(
     tags=["items"],
     responses={404: {"description": "Not found"}},
 )
+
+def get_db_2():
+    '''
+    I need this purely for the non-endpoint CRUD operations.
+    It's a hack but I'm getting desperate.
+    '''
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./pkce.db"
+    engine = create_engine(
+            SQLALCHEMY_DATABASE_URL,
+            connect_args={"check_same_thread": False},
+    )
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # Return the session
+    return session
+
+def non_endpoint_create_pkce(item: PKCEItemCreate): # type: ignore
+    db = get_db_2()
+    result = PKCEService(db).create_item(item)
+    return handle_result(result)
+
+def non_endpoint_find_state(state: str):
+    db = get_db_2()
+    result = PKCEService(db).get_item(state)
+    return handle_result(result)
+
 
 @router.post("/state/", response_model=PKCEItem)
 async def create_item(item: PKCEItemCreate, db: get_db = Depends()): # type: ignore
